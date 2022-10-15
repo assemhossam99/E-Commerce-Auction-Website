d6d72c8f322367c75d510a0f9dfbbf5ec8b63f7f
Assem <assem99.hossam@gmail.com>
2022-10-15 02:30:53 +0200

    added listing, comment and bid tables to DB

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,30 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length = 64)
+    description = models.CharField(max_length = 1024)
+    image_url = models.CharField(max_length = 1024, blank = True)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.title} -> {self.price}"
+
+class Bid(models.Model):
+    price = models.IntegerField()
+    date = models.DateField()
+    listing = models.ForeignKey(Listing, on_delete = models.CASCADE, related_name = "bids")
+    # user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "bids")
+
+    def __str__(self):
+        return f"{self.id}: bid with value ({self.price}$) on item {self.listing.id}"
+
+class Comment(models.Model):
+    calue = models.CharField(max_length = 1024)
+    date = models.DateField()
+    listing = models.ForeignKey(Listing, on_delete = models.CASCADE, related_name = "comments")
+    # user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "comments")
+
+    def __str__(self):
+        return f"{self.id}: comment ({self.value}$) on item {self.listing.id}"
